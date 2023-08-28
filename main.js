9dd5fe7a0b8856bb2fb6a1c86a038d1cb143a159
Mikail Thoriq <mikail.thoriq@gmail.com>
2023-08-28 20:58:27 +0700

    change cdn proxy

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-import './style.css';
 import { theBackground } from './src/theBackground';
 import startStarryBackground from './src/man-thats-beautiful';
 
+// inject random radial-gradient 
 window.onload = function() {
   document.body.style.backgroundImage = theBackground();
 };
--- src/getRandomColor.js
+++ src/getRandomColor.js
@@ -1,2 +1,2 @@
 export const getRandomColor = () => 
-    '#' + ('00000' + ((Math.random() * (1 << 24)) || 0).toString(16)).slice(-6)
+    '#' + ('00000' + ((Math.random() * (1 << 24)) || 0).toString(16)).slice(-6);
